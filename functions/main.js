33c6ed5a744f99f53097be4cf3a48359bdde6278
ParkYoungWoong <thesecon@gmail.com>
2021-01-02 10:42:58 +0900

    Add cors wildcard to netlify function

--- functions/main.js
+++ functions/main.js
@@ -21,7 +21,10 @@ exports.handler = async function (event) {
   return {
     statusCode: 200,
     headers: {
-      'Content-Type': 'application/json; charset=UTF-8'
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET'
     },
     body: JSON.stringify(jsonFiles[requestName])
   };
